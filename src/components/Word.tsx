6c1d9cccfde378f868c223a36e879f1bfbc2b5e1
cycsd <cycsd9211@gmail.com>
2023-07-05 14:42:42 +0800

    tab defaultactivekey

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,4 +1,4 @@
-import { For, Match, Show, Switch, createResource, createSignal } from "solid-js";
+import { For, Index, Match, Show, Switch, createResource, createSignal } from "solid-js";
 import { useWordContext } from "../context/WordContext"
 import { Card, Col, Nav, Row, Tab } from "solid-bootstrap";
 
@@ -60,61 +60,58 @@ export function WordCard() {
         let url = new URL('https://localhost:7186/api/Vocabulary');
         if (wordContext?.word() != undefined && wordContext.word() != "") {
             url.searchParams.set('word', wordContext?.word());
-            console.log(url.toString());
-            const res = await fetch(url.toString());
+            //console.log(url.toString());
+            const res = await fetch(url.toString()).then((res) => {
+                if (res.ok) { return res; } else { throw new Error(res.statusText) }
+            });
             return res.json();
         }
         else { throw new Error("bad request"); }
 
     }
     const [vocabulary] = createResource<SimpleWordInfoDto, string>(wordContext?.word, fetchDefinition);
-    const getPronounceAudio = ()=>{
+    const getPronounceAudio = () => {
         return new Audio(vocabulary()?.pronounceAudioUrl);
     }
     let play_pronounce = (e: Event) => {
         let audio = getPronounceAudio();
-        //audio.load();
         audio.play();
-        console.log(audio.src)
-        console.log("play");
     };
 
-    return (<div><Switch fallback={<p>{vocabulary.state}{vocabulary.error} show off</p>}>
-        <Match when={vocabulary.state === 'ready'}>
-            <h2 class="mx-3">{vocabulary()?.text}</h2>
-            <audio controls>
-                <source src={vocabulary()?.pronounceAudioUrl}></source>
-            </audio>
-            <span onclick={play_pronounce} class="material-symbols-outlined btn mx-3">
-                play_arrow
-            </span>
-            <Tab.Container>
-                <Nav variant="pills" class="grid grid-cols-6 gap-1">
-                    <For each={vocabulary()?.partOfSpeech}>{
-                        (poc) => (
-                            <Nav.Item>
-                                <Nav.Link eventKey={poc.partOfSpeech}>{poc.partOfSpeech}</Nav.Link>
-                            </Nav.Item>
-                        )}
-                    </For>
-                </Nav>
-                <Tab.Content>
-                    <For each={vocabulary()?.partOfSpeech}>{
-                        (poc) => (
-                            <Tab.Pane eventKey={poc.partOfSpeech}>
-                                <For each={poc.definitions}>{
-                                    (def) => (
-                                        <p>-{def}</p>
-                                    )
-                                }
-                                </For>
-                            </Tab.Pane>
-                        )}
-                    </For>
-                </Tab.Content>
-            </Tab.Container >
-        </Match>
-    </Switch>
+    return (<div>
+        <Switch>
+            <Match when={vocabulary.state === 'ready'}>
+                <h2 class="mx-3">{vocabulary()?.text}</h2>
+                <span onclick={play_pronounce} class="material-symbols-outlined btn mx-3">
+                    play_arrow
+                </span>
+                <Tab.Container defaultActiveKey={0}>
+                    <Nav variant="pills" class="grid grid-cols-6 gap-1">
+                        <For each={vocabulary()?.partOfSpeech}>{
+                            (poc, index) => (
+                                <Nav.Item>
+                                    <Nav.Link eventKey={index()}>{poc.partOfSpeech}</Nav.Link>
+                                </Nav.Item>
+                            )}
+                        </For>
+                    </Nav>
+                    <Tab.Content>
+                        <For each={vocabulary()?.partOfSpeech}>{
+                            (poc, index) => (
+                                <Tab.Pane eventKey={index()}>
+                                    <For each={poc.definitions}>{
+                                        (def) => (
+                                            <p>-{def}</p>
+                                        )
+                                    }
+                                    </For>
+                                </Tab.Pane>
+                            )}
+                        </For>
+                    </Tab.Content>
+                </Tab.Container >
+            </Match>
+        </Switch>
     </div>)
 
 
